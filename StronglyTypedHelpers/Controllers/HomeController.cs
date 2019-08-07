04e4ac6f129675326dedc057747203b659fdf678
vasiliv <ggo24@yahoo.com>
2019-08-07 18:25:56 +0400

    Add project files.

---
+++ StronglyTypedHelpers/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using StronglyTypedHelpers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StronglyTypedHelpers.Controllers {
+    public class HomeController : Controller {
+        Employee emp = new Employee() {
+            Address = "Georgia", Name = "Vasil", IsEmployee = true
+        };
+        public ActionResult Index() {
+            return View(emp);
+        }
+        [HttpPost]
+        public ActionResult Index(Employee emp) {
+            return View(emp);
+        }
+    }
+}
---
+++ StronglyTypedHelpers/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StronglyTypedHelpers.Models {
+    public class Employee {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public bool IsEmployee { get; set; }
+    }
+}
